c02ce15b7b9346a2bef25552daf4ca00bc9170ec
summerpro <974741468@qq.com>
2021-07-09 16:44:05 +0800

    udpate package.json, add ut

---
+++ packages/transactions/lib.esm/test.js
@@ -0,0 +1,12 @@
+import { parse } from "./index";
+import assert from "assert";
+describe('Test parse', function () {
+    it('parses transaction function', function () {
+        // Transaction: 0x820cc57bc77be44d8f4f024a18e18f64a8b6e62a82a3d7897db5970dbe181ba1
+        let rawTx = "0xf86c808405f5e10082520894f76b10a1f318825173ad9f83f112e570782bd83e893635c9adc5dea000008033a0d65f035fdf5ca18659445ec384aa775280b86d9ca7c4e352ea02a660536bade6a0095fc765cb672e54069672eba41f6488b73ce7d235f113b537f8860f87928963";
+        let tx = parse(rawTx);
+        console.log(tx);
+        assert.equal(tx.hash, "0xbe648799632a6db88b81fdd48830087f2695f6190c63771e732f29a565b82b56");
+    });
+});
+//# sourceMappingURL=test.js.map
